9f792b94c02a1e18d67fe7832d1be9ac80088530
sarthak0417 <146656539+sarthak0417@users.noreply.github.com>
2023-12-02 19:21:17 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -14,7 +14,7 @@ int main()
     printf("enter subject5 marks");
     scanf("%d", &s5);
     totalmarks = s1+s2+s3+s4+s5;
-    percentage = (totalmarks / (5*100)) * 100;
+    percentage = (totalmarks*100)/500;
     printf("PERCENTAGE AQQUIRED : %d\n", percentage);
     return 0;
 }
